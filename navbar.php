4163ea0a5e7be2d9316c61a084fcc3cc66aa8dd3
FARIKAH17 <hallosey801@gmail.com>
2025-11-13 00:32:33 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,1 +1,24 @@
+<!-- navbar.php -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Admin Dashboard</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="../makanan/index.php">🍛 Makanan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../minuman/index.php">🍸 Minuman</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../pesanan/index.php">🧾 Pesanan</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
