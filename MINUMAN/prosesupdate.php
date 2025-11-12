1f315124a37529f350bf79d8e90400ca14584cc6
FARIKAH17 <hallosey801@gmail.com>
2025-11-13 00:30:32 +0800

    Update prosesupdate.php

--- MINUMAN/prosesupdate.php
+++ MINUMAN/prosesupdate.php
@@ -1,1 +1,18 @@
+<?php
+include '../koneksi.php';
+$id = $_POST['id'];
+$nama = $_POST['nama'];
+$harga = $_POST['harga'];
+$stok = $_POST['stok'];
+
+if (!empty($_FILES["image"]["name"])) {
+    $image = basename($_FILES["image"]["name"]);
+    move_uploaded_file($_FILES["image"]["tmp_name"], "uploads/" . $image);
+    $conn->query("UPDATE minuman SET nama='$nama', harga='$harga', stok='$stok', image='$image' WHERE id=$id");
+} else {
+    $conn->query("UPDATE minuman SET nama='$nama', harga='$harga', stok='$stok' WHERE id=$id");
+}
+
+header("Location: index.php");
+?>
 
